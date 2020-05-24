2c4946c8861b5e15c2bf5c0310b1b4e3f0f0ad0e
lianginger <jackyliang444@hotmail.com.tw>
2020-05-25 00:29:13 +0800

    Updates

---
+++ precache-manifest.a78547a4be4d942f2454d7d130179621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcd4c6f216d6b88a24a1b545ab04847",
+    "url": "/facebook-ad-account-dashboard/index.html"
+  },
+  {
+    "revision": "43365f89d49d1623e66d",
+    "url": "/facebook-ad-account-dashboard/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "43365f89d49d1623e66d",
+    "url": "/facebook-ad-account-dashboard/static/js/2.34c6643e.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/facebook-ad-account-dashboard/static/js/2.34c6643e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c72eb4ea626f63b8a7",
+    "url": "/facebook-ad-account-dashboard/static/js/main.4555346b.chunk.js"
+  },
+  {
+    "revision": "b58938b7ce639180c509",
+    "url": "/facebook-ad-account-dashboard/static/js/runtime-main.dba334fc.js"
+  }
+]);
