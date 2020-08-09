7447786df16cde3361996ad260fe8f9cab8b20ed
lianginger <jackyliang444@hotmail.com.tw>
2020-08-09 14:42:41 +0800

    Updates

---
+++ precache-manifest.cf4a898ece5584fcc53215ad71ac5d15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa68701bb7910436dead74b664dc8e03",
+    "url": "/facebook-ad-account-dashboard/index.html"
+  },
+  {
+    "revision": "a5e0b5a9bf3572e418db",
+    "url": "/facebook-ad-account-dashboard/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b484e985454d1cb9417d",
+    "url": "/facebook-ad-account-dashboard/static/css/main.2eea0fac.chunk.css"
+  },
+  {
+    "revision": "a5e0b5a9bf3572e418db",
+    "url": "/facebook-ad-account-dashboard/static/js/2.8e25d64c.chunk.js"
+  },
+  {
+    "revision": "f8a8b806455b53cbdd585b03974fb114",
+    "url": "/facebook-ad-account-dashboard/static/js/2.8e25d64c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b484e985454d1cb9417d",
+    "url": "/facebook-ad-account-dashboard/static/js/main.674ad94a.chunk.js"
+  },
+  {
+    "revision": "b58938b7ce639180c509",
+    "url": "/facebook-ad-account-dashboard/static/js/runtime-main.dba334fc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/facebook-ad-account-dashboard/precache-manifest.f21b0c3180234a0325dfa60bcfe8846e.js"
+  "/facebook-ad-account-dashboard/precache-manifest.cf4a898ece5584fcc53215ad71ac5d15.js"
 );
 
 self.addEventListener('message', (event) => {
